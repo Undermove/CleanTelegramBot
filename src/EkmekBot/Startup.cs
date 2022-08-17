f80dcd81a2d820d6aa4a9cba99b31bc098ce6fa4
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2022-08-17 19:02:22 +0400

    Remove old useless references

--- src/EkmekBot/Startup.cs
+++ src/EkmekBot/Startup.cs
@@ -53,8 +53,6 @@ public class Startup
         }
 
         app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EkmekBot v1"));
 
         app.UseRouting();
 
